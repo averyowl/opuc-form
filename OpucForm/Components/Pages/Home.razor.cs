85ec4d2c5317929a1ab621e80867ad2d896d5e27
Hunter Gambee-Iddings <hunterg@cecs.pdx.edu>
2025-11-06 22:17:49 -0800

    Add full form HTML structure and Sass-based styling

---
+++ OpucForm/Components/Pages/Home.razor.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace OpucForm.Components.Pages
+{
+    public partial class Home : ComponentBase
+    {
+
+        [Inject] private IJSRuntime JS { get; set; } = default!;
+
+
+        private bool SameAsHomeAddress { get; set; } = true;
+        private string? QualificationProgram { get; set; } = "";
+        private IJSObjectReference? _mapboxModule;
+        private string _ssn = "";
+        private string? SelectedFileName;
+
+
+        public string SSN
+
+        {
+            get => _ssn;
+            set
+            {
+                // Remove non-digits
+                var digits = new string(value.Where(char.IsDigit).ToArray());
+
+                // Limit to 9 digits
+                if (digits.Length > 9)
+                    digits = digits[..9];
+
+                // Add dashes
+                if (digits.Length > 5)
+                    _ssn = $"{digits[..3]}-{digits.Substring(3, 2)}-{digits[5..]}";
+                else if (digits.Length > 3)
+                    _ssn = $"{digits[..3]}-{digits[3..]}";
+                else
+                    _ssn = digits;
+            }
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                _mapboxModule = await JS.InvokeAsync<IJSObjectReference>("import", "./js/mapbox.js");
+                await _mapboxModule.InvokeVoidAsync("initMapbox");
+                
+            }
+        }
+
+
+        private async Task OnMailingAddressToggleChanged()
+        {
+            if (!SameAsHomeAddress && _mapboxModule is not null)
+            {
+                // Re-initialize Mapbox autofill for mailing address fields
+                await _mapboxModule.InvokeVoidAsync("initMailingAddressAutocomplete");
+            }
+        }
+
+        private void OnFileSelected(InputFileChangeEventArgs e)
+        {
+            SelectedFileName = e.FileCount > 0
+                ? e.File.Name
+                : null;
+        }
+
+
+    }
+}
---
+++ OpucForm/README.md
@@ -0,0 +1,40 @@
+﻿# OPUC Lifeline Application
+
+This project is a Blazor Server web application for the **Oregon Public Utility Commission (OPUC)** Lifeline Online Application and Undeliverable Mail Automation project.
+
+## Prerequisites
+
+Before running the app, make sure you have the following installed:
+
+- [.NET 9 SDK](https://dotnet.microsoft.com/download/dotnet/9.0)
+- [Sass](https://sass-lang.com/install) – used to compile `.scss` to `.css`
+
+
+## Running the Project
+
+In two terminals, run the following commands:
+
+### 1️⃣ Start the Blazor Server app
+```bash
+dotnet watch run
+```
+This will launch the app in watch mode. It rebuilds automatically when code changes.
+
+### 2️⃣ Start the Blazor Server app
+```bash
+sass --watch wwwroot/scss/main.scss:wwwroot/css/styles.css --style=expanded
+
+```
+
+This continuously watches your SCSS files and recompiles them to styles.css whenever you make changes.
+
+## Notes
+* The compiled CSS file should not be manually edited - always modify .scss files.
+* If you encounter issues with the Sass command, ensure sass is in your system path:
+```bash
+sass --version
+```
+* You can also install Sass via npm if needed:
+```bash
+npm install -g sass
+```
